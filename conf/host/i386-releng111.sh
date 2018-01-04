c0d1d95f15997a2fd613c15ad8408397350921b5
Hiroki Tagato <tagattie@gmail.com>
2018-01-04 19:53:05 +0900

    Update i386 configurations.

--- conf/host/i386-releng111.sh
+++ conf/host/i386-releng111.sh
@@ -1,22 +1,29 @@
 #! /bin/sh
 
+ARCH=i386
+BRANCH=releng111
+
 # source common variables and functions
 # shellcheck source=../common.sh
 . "${CONFDIR}/common.sh"
 # shellcheck source=../arch/i386.sh
-. "${CONFDIR}/arch/i386.sh"
+. "${CONFDIR}/arch/${ARCH}.sh"
 # shellcheck source=../branch/releng111.sh
-. "${CONFDIR}/branch/releng111.sh"
+. "${CONFDIR}/branch/${BRANCH}.sh"
 
-export DESTHOST=i386-releng111
-export CAPDDESTHOST=I386-Releng111
+export DESTHOST=${ARCH}-${BRANCH}
+CAPDDESTHOST=$(echo ${DESTHOST} | \
+    awk '{print toupper(substr($0,1,1)) substr($0,2,length($0)-1)}')
+export CAPDDESTHOST
 export KERNCONF=GENERIC
 
-export OBJDIR=/var/tmp/jenkins/freebsd/obj/releng111
+export OBJDIR_BASEDIR=/var/tmp/jenkins/freebsd/obj
+export OBJDIR=${OBJDIR_BASEDIR}/${DESTHOST}
 
-export DESTROOT_BASEDIR=/var
+export DESTROOT_BASEDIR=/
 export DESTROOT_MOUNTTYPE=zfs
-export DESTDIR=${DESTROOT_BASEDIR}/tmp/jenkins/freebsd/destdir/${DESTHOST}
+export DESTDIR_BASEDIR=/var/tmp/jenkins/freebsd/destdir
+export DESTDIR=${DESTROOT_BASEDIR}${DESTDIR_BASEDIR}/${DESTHOST}
 
 export DESTROOTDIR=${DESTDIR}${ROOTDIR}
 
