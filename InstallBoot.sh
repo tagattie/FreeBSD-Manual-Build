0c21df4d640354e8788683acf14825d04e39e7ca
Hiroki Tagato <tagattie@gmail.com>
2018-01-07 06:40:10 +0900

    Take an option -c to pass extra configuration.

--- InstallBoot.sh
+++ InstallBoot.sh
@@ -3,35 +3,49 @@
 export LANG=C
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
-LOCALBASE=/usr/local
-CONFDIR=$(pwd)/conf
+BASEDIR=$(cd "$(dirname "$0")" && pwd)
+CONFDIR=${BASEDIR}/conf
 
-CMDNAME=$(basename $0)
+CMDNAME=$(basename "$0")
 
 print_usage() {
-    echo "Usage: ${CMDNAME} -?|-h hostname"
+    echo "Usage: ${CMDNAME} [-?|-h hostname [-c file]]"
     echo "Options:"
     echo "  -?: Show this message."
+    echo "  -c: Extra configuration file (for overriding defaults)."
     echo "  -h: Target hostname."
     exit 0
-}
+} # print_usage()
 
 main() {
-    while getopts ?h: OPT; do
-        case ${OPT} in
-            "?")
-                print_usage ;;
-            "h")
-                DESTHOST=${OPTARG} ;;
-        esac
-    done
+    if [ $# -eq 0 ]; then
+        print_usage
+    else
+        while getopts \?h:c: OPT; do
+            case ${OPT} in
+                "?")
+                    print_usage ;;
+                "c")
+                    EXTRA_CONF=${OPTARG} ;;
+                "h")
+                    DESTHOST=${OPTARG} ;;
+            esac
+        done
+    fi
+
     if [ -z "${DESTHOST}" ]; then
         echo "${CMDNAME}: You must specify a target hostname with -h."
         exit 1
     fi
-    . ${CONFDIR}/${DESTHOST}.conf
+
+    . "${CONFDIR}/host/${DESTHOST}.sh"
+    if [ -n "${EXTRA_CONF}" ]; then
+        . "${EXTRA_CONF}"
+    fi
+
     install_boot
+
     return 0
-}
+} # main()
 
 main "${@}"
