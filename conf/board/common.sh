e1d51cc4fab012e9ac180b95e142e9d404528e24
Hiroki Tagato <tagattie@gmail.com>
2018-01-06 23:01:54 +0900

    Add board-specific configuration for RPI3.

---
+++ conf/board/common.sh
@@ -0,0 +1,49 @@
+#! /bin/sh
+
+# Basic units
+export KiB=1024
+export MiB=$((KiB*1024))
+export GiB=$((MiB*1024))
+
+# Traditional logical disk structure
+export SECTORS_PER_TRACK=63
+export HEADS_PER_CYLINDER=255
+export BYTES_PER_SECTOR=512
+export START_SECTOR=63
+
+# Defaults for disk image file (possibly be overridden)
+export IMG_SIZE=$((1*GiB))
+export IMG_SIZEMB=$((IMG_SIZE/MiB))
+
+# Default parition scheme is MBR
+export PART_SCHEME=MBR
+
+# Defaults for boot partition (possibly be overridden)
+export BOOT_PART_SIZE=$((16*MiB))
+export BOOT_PART_SIZEMB=$((BOOT_PART_SIZE/MiB))
+
+export BOOT_PART_SECTOR_MARGIN=1
+export BOOT_PART_TYPE='!12'       # FAT32 (LBA-addressable)
+export BOOT_PART_LABEL=msdosboot
+export BOOT_PART_FSTYPE=msdosfs
+export BOOT_PART_FS=16            # FAT16
+
+# Defaults for BSD partition
+export BSD_PART_TYPE=freebsd
+export BSD_PART_SCHEME=BSD
+export BSD_PART_FSTYPE=freebsd-ufs
+export BSD_PART_FSALIGN=$((64*KiB))
+export BSD_PART_FSENDIAN=big
+export BSD_PART_FSLABEL=rootfs
+BSD_PART_FSMINFREE=$(bc -e "${IMG_SIZE}/1024*0.5" -e quit | \
+                         awk -F'.' '{print $1}')
+export BSD_PART_FSMINFREE
+export BSD_PART_FSMARGIN=$((1*MiB))
+export BSD_PART_FSSIZE=$(((IMG_SIZEMB-BOOT_PART_SIZEMB)*MiB-BSD_PART_FSMARGIN))
+
+# Defaults for overlay files
+export OVERLAY_FILES="boot/loader.conf etc/fstab etc/rc.conf"
+export OVERLAY_FILE_MODE=644
+export OVERLAY_FILE_OWNER=root
+export OVERLAY_FILE_GROUP=wheel
+export FIRST_BOOT_SENTINEL=/firstboot
---
+++ conf/board/rpi3.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+# source common variables for boards
+# shellcheck source=./common.sh
+. "${CONFDIR}/board/common.sh"
+
+export BOARD_NAME=rpi3
+
+# Image size is 3GiB
+export IMG_SIZE=$((3*GiB))
+export IMG_SIZEMB=$((IMG_SIZE/MiB))
+# Boot partition size is 32MiB
+export BOOT_PART_SIZE=$((32*MiB))
+export BOOT_PART_SIZEMB=$((BOOT_PART_SIZE/MiB))
+
+do_post_installworld() {
+    BOOT_FILES="boot1.efi"
+    echo "${CMDNAME}: Copying boot files to EFI partition."
+    mkdir -p "${BOOTEFIDIR_DEST}/EFI/BOOT"
+    install -c "${BOOTDIR_DEST}/${BOOT_FILES}" "${BOOTEFIDIR_DEST}/EFI/BOOT/bootaa64.efi"
+    return 0
+}
+
+install_boot() {
+    echo "${CMDNAME}: Copying boot files to EFI partition."
+    UBOOT_MASTERDIR=${LOCALBASE}/share/u-boot/u-boot-rpi3
+    UBOOT_FILES="armstub8.bin bootcode.bin config.txt \
+        fixup.dat fixup_cd.dat fixup_x.dat \
+        start.elf start_cd.elf start_x.elf \
+        u-boot.bin"
+    DTB_REPO="https://github.com/raspberrypi/firmware/blob/master/boot"
+    DTB_FILES="bcm2710-rpi-3-b.dtb \
+        overlays/mmc.dtbo overlays/pi3-disable-bt.dtbo"
+    for i in ${UBOOT_FILES}; do
+        install -c "${UBOOT_MASTERDIR}/${i}" "${BOOTEFIDIR_DEST}"
+    done
+    mkdir -p "${BOOTEFIDIR_DEST}/overlays"
+    (cd "${BOOTEFIDIR_DEST}" && rm -f "${DTB_FILES}")
+    for i in ${DTB_FILES}; do
+        fetch -o "${BOOTEFIDIR_DEST}/${i}" "${DTB_REPO}/${i}?raw=true"
+    done
+    return 0
+}
