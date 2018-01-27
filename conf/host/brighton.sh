c29db0a6aba15a704757d2adcc825c49a9951100
Hiroki Tagato <tagattie@gmail.com>
2018-01-27 20:12:40 +0900

    Add configurations for brighton.

---
+++ conf/host/brighton.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+ARCH=amd64
+BRANCH=releng111
+
+# source common variables and functions
+# shellcheck source=../common.sh
+. "${CONFDIR}/common.sh"
+# shellcheck source=../arch/amd64.sh
+. "${CONFDIR}/arch/${ARCH}.sh"
+# shellcheck source=../branch/releng111.sh
+. "${CONFDIR}/branch/${BRANCH}.sh"
+
+export DESTHOST=brighton
+CAPDDESTHOST=$(echo ${DESTHOST} | \
+    awk '{print toupper(substr($0,1,1)) substr($0,2,length($0)-1)}')
+export CAPDDESTHOST
+KERNCONF=$(echo ${DESTHOST}|tr '[:lower:]' '[:upper:]')
+export KERNCONF
+
+export DESTDIR_BASEDIR=/mnt
+export DESTDIR_MOUNTTYPE=nfs
+export DESTDIR=${DESTDIR_BASEDIR}/${DESTHOST}
+
+MOUNT_TARGET_DIRS="/ /home /tmp \
+    /usr /usr/local /usr/local/man /usr/share /usr/share/man \
+    /var /var/audit /var/crash /var/db /var/empty /var/log /var/mail \
+    /var/run /var/tmp"
+MOUNT_TARGETS=$(for i in ${MOUNT_TARGET_DIRS}; do
+                    echo "${DESTHOST}:${i}:${DESTDIR_MOUNTTYPE}:${DESTDIR}"
+                done)
+export MOUNT_TARGETS
