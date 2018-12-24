2ad196570863e02fdffe1e3c6f36c6745634f340
Hiroki Tagato <tagattie@gmail.com>
2018-12-24 18:09:08 +0900

    Update to releng/12.0.

--- conf/host/parkcity.sh
+++ conf/host/parkcity.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 ARCH=amd64
-BRANCH=releng112
+BRANCH=releng120
 
 # source common variables and functions
 # shellcheck source=../common.sh
 . "${CONFDIR}/common.sh"
 # shellcheck source=../arch/amd64.sh
 . "${CONFDIR}/arch/${ARCH}.sh"
-# shellcheck source=../branch/releng112.sh
+# shellcheck source=../branch/releng120.sh
 . "${CONFDIR}/branch/${BRANCH}.sh"
 
 export DESTHOST=parkcity
