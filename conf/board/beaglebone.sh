88d9e84dce5c49aa10d8cd98440c742d18014f61
Hiroki Tagato <tagattie@gmail.com>
2018-12-28 16:50:43 +0900

    Update to releng/12.0.

--- conf/board/beaglebone.sh
+++ conf/board/beaglebone.sh
@@ -6,8 +6,6 @@
 
 export BOARD_NAME=beaglebone
 
-export UBLDR_LOADADDR=0x88000000
-
 # Image size is 1GiB
 export IMG_SIZE=$((1*GiB))
 export IMG_SIZEMB=$((IMG_SIZE/MiB))
@@ -16,35 +14,43 @@ export BOOT_PART_SIZE=$((16*MiB))
 export BOOT_PART_SIZEMB=$((BOOT_PART_SIZE/MiB))
 
 do_post_installkernel() {
-    echo "${CMDNAME}: Making necessary DTB file hard links."
-    (cd "${BOOTDIR_DEST}/dtb" && \
-         ${SUDO} ln -f beaglebone.dtb am335x-bone.dtb &&
-         ${SUDO} ln -f beaglebone-black.dtb am335x-boneblack.dtb)
+    echo "${CMDNAME}: Copying DTB files to FAT partition."
+    DTB_DIR="dtb"
+    ${SUDO} mkdir -p "${BOOTFATDIR_DEST}/${DTB_DIR}"
+    ${SUDO} rsync -rlDv --stats "${BOOTDIR_DEST}/${DTB_DIR}/" "${BOOTFATDIR_DEST}/${DTB_DIR}"
+
     return 0
 }
 
 do_post_installworld() {
-    UBLDR_FILES="ubldr ubldr.bin"
-    echo "${CMDNAME}: Copying loader files to FAT partition."
+    echo "${CMDNAME}: Copying u-boot loader files to FAT partition."
+    UBLDR_FILES="ubldr.bin"
     ${SUDO} mkdir -p "${BOOTFATDIR_DEST}"
     for i in ${UBLDR_FILES}; do
         ${SUDO} ${INSTALL_FILE} \
                 "${BOOTDIR_DEST}/${i}" \
                 "${BOOTFATDIR_DEST}"
     done
+
+    echo "${CMDNAME}: Copying loader file to FAT partition."
+    BOOT_FILE="loader_lua.efi"
+    ${SUDO} mkdir -p "${BOOTEFIDIR_DEST}/EFI/BOOT"
+    ${SUDO} ${INSTALL_FILE} "${BOOTDIR_DEST}/${BOOT_FILE}" "${BOOTEFIDIR_DEST}/EFI/BOOT/bootarm.efi"
+
     return 0
 }
 
 install_boot() {
     echo "${CMDNAME}: Copying boot files to FAT partition."
     UBOOT_MASTERDIR=${LOCALBASE}/share/u-boot/u-boot-beaglebone
-    UBOOT_FILES="MLO boot.scr u-boot.img"
+    UBOOT_FILES="MLO u-boot.img"
     ${SUDO} mkdir -p "${BOOTFATDIR_DEST}"
     for i in ${UBOOT_FILES}; do
         ${SUDO} ${INSTALL_FILE} \
                 "${UBOOT_MASTERDIR}/${i}" \
                 "${BOOTFATDIR_DEST}"
     done
+
     return 0
 }
 
@@ -60,5 +66,6 @@ populate_boot_partition() {
 
 create_placeholder_for_boot_partition() {
     ${SUDO} mkdir -p "${WORKDIR}/${BSD_PART_FSLABEL}/${BOOTFATDIR}"
+
     return 0
 }
