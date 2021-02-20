abb41e9dcf7ec0edb885c4a49f3c81c6a75169b1
Hiroki Tagato <tagattie@gmail.com>
2021-02-20 15:35:16 +0900

    aspen: update to releng/12.2.

--- conf/host/aspen.sh
+++ conf/host/aspen.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 ARCH=i386
-BRANCH=releng120
+BRANCH=releng122
 
 # source common variables and functions
 # shellcheck source=../common.sh
 . "${CONFDIR}/common.sh"
 # shellcheck source=../arch/i386.sh
 . "${CONFDIR}/arch/${ARCH}.sh"
-# shellcheck source=../branch/releng120.sh
+# shellcheck source=../branch/releng122.sh
 . "${CONFDIR}/branch/${BRANCH}.sh"
 
 export DESTHOST=aspen
