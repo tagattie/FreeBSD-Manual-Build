ad1647934c7a7e0833c645dfcd3a265c850e6b85
Hiroki Tagato <tagattie@gmail.com>
2023-05-15 17:49:20 +0900

    steamboat: update to releng/13.2

--- conf/host/steamboat.sh
+++ conf/host/steamboat.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 ARCH=amd64
-BRANCH=releng131
+BRANCH=releng132
 
 # source common variables and functions
 # shellcheck source=../common.sh
 . "${CONFDIR}/common.sh"
 # shellcheck source=../arch/amd64.sh
 . "${CONFDIR}/arch/${ARCH}.sh"
-# shellcheck source=../branch/releng131.sh
+# shellcheck source=../branch/releng132.sh
 . "${CONFDIR}/branch/${BRANCH}.sh"
 
 export DESTHOST=steamboat
