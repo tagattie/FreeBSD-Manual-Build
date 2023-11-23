5acb9af8bb29c09b70ed2aa6d42a8a5d2cf18207
Hiroki Tagato <tagattie@gmail.com>
2023-11-23 14:04:25 +0900

    revelstoke: update to releng/14.0

--- conf/host/revelstoke.sh
+++ conf/host/revelstoke.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 ARCH=amd64
-BRANCH=releng132
+BRANCH=releng140
 
 # source common variables and functions
 # shellcheck source=../common.sh
 . "${CONFDIR}/common.sh"
 # shellcheck source=../arch/amd64.sh
 . "${CONFDIR}/arch/${ARCH}.sh"
-# shellcheck source=../branch/releng132.sh
+# shellcheck source=../branch/releng140.sh
 . "${CONFDIR}/branch/${BRANCH}.sh"
 
 export DESTHOST=revelstoke
